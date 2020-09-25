a421382c53b5c95c17b6912e7bd69001a03a21fe
Cody Mailey <cmailey03@gmail.com>
2020-09-25 12:01:51 -0600

    Done with quiz

--- admission.java
+++ admission.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-;public class Admissions 
+;public class admission
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -7,11 +7,12 @@ import java.util.Scanner;
         int SATrank = 0;
         int rankclass = 0;
         int awarkrank = 0;
-        int residence = 0;
+        int rankresidence = 1;
 
         System.out.println("To see if you will be accepted into this college you must answer a few quetions");
         System.out.println("What is your SAT score?");
         int SAT = scanner.nextInt();
+
         if (SAT >=1500) {
             SATrank = 3;
         } else if (SAT < 1500 && SAT >= 1400) {
@@ -21,6 +22,7 @@ import java.util.Scanner;
         }
         System.out.println("What is your Class Rank?");
         int classrank = scanner.nextInt();
+
         if (classrank >=95) {
             rankclass = 3;
         } else if (classrank < 95 && classrank >= 90) {
@@ -31,18 +33,27 @@ import java.util.Scanner;
         
         System.out.println("how many awards have you won?");
         int awards = scanner.nextInt();
-        if (SAT >=1500) {
-            SATrank = 3;
-        } else if (SAT < 1500 && SAT >= 1400) {
-            SATrank = 2;
-        } else if (SAT < 1400 && SAT >= 1200) {
-            SATrank = 1;
+
+        if (awards >=10) {
+            awarkrank = 3;
+        } else if (awards < 10 && awards >= 8) {
+            awarkrank = 2;
+        } else if (awards < 8 && awards >= 3) {
+            awarkrank = 1;
         }
         System.out.println("where are you from?");
-        int residence = scanner.nextInt();
+        String residence = scanner.next();
 
-        
+        if (residence.equals("colorado") ) {
+            rankresidence = 3;
+        } else if (residence.equals("florida")) {
+            rankresidence = 0;
+        }
 
-
+        if (rankresidence == 0 || rankclass == 0 || awarkrank == 0 || SATrank == 0) {
+            System.out.println("You will not be accepted to this college.");
+        } else if ((rankclass + rankresidence + awarkrank + SATrank) >= 6) {
+            System.out.println("You will be accepted to this college.");
+        }
     }
 }
